ffc39fd39ad82e7d386ad8208f767a92b05ce930
Erik Dubois <erik.dubois@gmail.com>
2025-04-14 20:38:08 +0200

    update

--- up.sh
+++ up.sh
@@ -3,14 +3,6 @@
 ##################################################################################################################
 # Author    : Erik Dubois
 # Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website   : https://www.ariser.eu
-# Website   : https://www.arcolinux.info
-# Website   : https://www.arcolinux.com
-# Website   : https://www.arcolinuxd.com
-# Website   : https://www.arcolinuxb.com
-# Website   : https://www.arcolinuxiso.com
-# Website   : https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
@@ -33,11 +25,6 @@
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
 git pull
-
-echo "getting picom.conf"
-installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
-found_file=$(find "$installed_dir" -type f -name "picom.conf" | head -n 1)
-wget -v https://raw.githubusercontent.com/arconetpro/picom/refs/heads/main/picom.conf -O $found_file 
 
 # Below command will backup everything inside the project folder
 git add --all .
